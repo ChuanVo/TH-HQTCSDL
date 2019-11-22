7298341271126cf4cdfbbdc75393137f0e3c24e8
lang.tran <1612331@student.hcmus.edu.vn>
2019-11-22 21:41:21 +0700

    DEADLOCK_TRUNGDUC

--- Data/Deadlock.sql
+++ Data/Deadlock.sql
@@ -84,5 +84,83 @@ EXEC PROC_DEADLOCK_T2_LANG 'dish_1', 'ag_1', 232
 
 --TrungDuc
 
+/*Giao tác A: 
+- Quản lý 1 xem tình trạng đơn hàng của id_bill  'X' (khi này sẽ giữ khóa đọc)
+- Delay 10s (Giao tác B thực hiện)
+- Quản lý 1 muốn cập nhật tình trạng đơn hàng cho  id_bill  'X' nhưng không thực hiện được (chờ giao tác B nhả khóa).
+Giao tác B:
+- Quản lý 2 xem tình trạng đơn hàng của id_bill  'X' (khi này sẽ giữ khóa đọc)
+- Delay 10s (Giao tác B thực hiện)
+- Quản lý 2 muốn cập nhật tình trạng đơn hàng cho  id_bill  'X' nhưng không thực hiện được (chờ giao tác B nhả khóa). Và giao tác B cũng không nhả khóa.
+=> Dẫn đến 2 tác chờ nhau và deadlock xảy ra => DMBS sẽ kill 1 giáo tác */
+
+--TRANSACTION 1 -- Quản lý 1 cập nhật tình trạng đơn hàng cho  id_bill  'X' tại chi nhánh 'Y'
+IF OBJECT_ID('PROC_DEADLOCK_T1_TRUNGDUC', N'P') IS NOT NULL DROP PROC PROC_DEADLOCK_T1_TRUNGDUC
+GO
+CREATE PROC PROC_DEADLOCK_T1_TRUNGDUC
+	@id_bill nchar(10),
+	@id_agency nchar(10),
+	@status nchar(10)
+AS
+BEGIN TRAN
+	SELECT * 
+	FROM BILL b WITH(HOLDLOCK)
+	WHERE b.id_bill = @id_bill and b.agency = @id_agency and b.isActive = 1
+	WAITFOR DELAY '00:00:10'
+
+	UPDATE BILL WITH(XLOCK)
+	SET status = @status
+	WHERE id_bill = @id_bill and agency = @id_agency and isActive = 1
+	
+COMMIT TRAN
+GO
+
+EXEC PROC_DEADLOCK_T1_TRUNGDUC N'bill_1', N'ag_1', N'sta_4'
+
+--TRANSACTION 2 --Quản lý 2 cập nhật tình trạng đơn hàng cho  id_bill  'X' tại chi nhánh 'Y'
+IF OBJECT_ID('PROC_DEADLOCK_T2_TRUNGDUC', N'P') IS NOT NULL DROP PROC PROC_DEADLOCK_T2_TRUNGDUC
+GO
+CREATE PROC PROC_DEADLOCK_T2_TRUNGDUC
+	@id_bill nchar(10),
+	@id_agency nchar(10),
+	@status nchar(10)
+AS
+BEGIN TRAN
+	SELECT * 
+	FROM BILL b WITH(HOLDLOCK)
+	WHERE b.id_bill = @id_bill and b.agency = @id_agency and b.isActive = 1
+	WAITFOR DELAY '00:00:10'
+
+	UPDATE BILL WITH(XLOCK)
+	SET status = @status
+	WHERE id_bill = @id_bill and agency = @id_agency and isActive = 1
+	
+COMMIT TRAN
+GO
+
+EXEC PROC_DEADLOCK_T2_TRUNGDUC N'bill_1', N'ag_1', N'sta_3'
+
+--TRANSACTION 2 FIX --Quản lý 2 cập nhật tình trạng đơn hàng cho  id_bill  'X' tại chi nhánh 'Y'
+IF OBJECT_ID('PROC_DEADLOCK_T2_TRUNGDUC', N'P') IS NOT NULL DROP PROC PROC_DEADLOCK_T2_TRUNGDUC
+GO
+CREATE PROC PROC_DEADLOCK_T2_TRUNGDUC
+	@id_bill nchar(10),
+	@id_agency nchar(10),
+	@status nchar(10)
+AS
+BEGIN TRAN
+	SELECT * 
+	FROM BILL b WITH(NOLOCK)
+	WHERE b.id_bill = @id_bill and b.agency = @id_agency and b.isActive = 1
+	WAITFOR DELAY '00:00:10'
+
+	UPDATE BILL WITH(XLOCK)
+	SET status = @status
+	WHERE id_bill = @id_bill and agency = @id_agency and isActive = 1
+	
+COMMIT TRAN
+GO
+
+EXEC PROC_DEADLOCK_T2_TRUNGDUC N'bill_1', N'ag_1', N'sta_3'
 --DangLam
 
