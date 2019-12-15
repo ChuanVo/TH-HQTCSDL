34ba992accc54b0c985d0b8f353c4eb9921de6fd
truonglam103 <truonglam103@gmail.com>
2019-12-15 11:40:18 +0700

    Update DirtyRead.sql
    Update dirty read

--- Data/DirtyRead.sql
+++ Data/DirtyRead.sql
@@ -86,43 +86,45 @@ GO
 EXEC PROC_DIRTYREAD_T1_LANG 'dish_5', ''
 
 --TRANSACTION 2 --
+--TRANSACTION 2 --
 IF OBJECT_ID('PROC_DIRTYREAD_T2_LANG', N'P') IS NOT NULL DROP PROC PROC_DIRTYREAD_T2_LANG
 GO
 CREATE PROC PROC_DIRTYREAD_T2_LANG
-	@id_dish nchar(10)
+	@id_agency nchar(10)
 AS
 BEGIN TRAN
 	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
-	SELECT *
-	FROM DISH 
-	WHERE id_dish = @id_dish
+	SELECT M.id_agency, D.id_dish, M.unit, D.dish_name, D.type_dish, D.image, D.price
+	FROM MENU M JOIN DISH D 
+	ON id_agency = @id_agency AND M.id_dish = D.id_dish AND M.isActive = 1
 COMMIT TRAN
 GO
 
-EXEC PROC_DIRTYREAD_T2_LANG 'dish_5'
+----EXEC PROC_DIRTYREAD_T2_LANG 'dish_5'
 
 -- TRANSACTION 2 FIX --
 IF OBJECT_ID('PROC_DIRTYREAD_T2_LANG', N'P') IS NOT NULL DROP PROC PROC_DIRTYREAD_T2_LANG
 GO
 CREATE PROC PROC_DIRTYREAD_T2_LANG
-	@id_dish nchar(10)
+	@id_agency nchar(10)
 AS
 BEGIN TRAN
 	SET TRANSACTION ISOLATION LEVEL READ COMMITTED
-	SELECT *
-	FROM DISH with (RepeatableRead)
-	WHERE id_dish = @id_dish
+	SELECT M.id_agency, D.id_dish, M.unit, D.dish_name, D.type_dish, D.image, D.price
+		FROM MENU M JOIN DISH D with (RepeatableRead)
+		ON id_agency = @id_agency AND M.id_dish = D.id_dish AND M.isActive = 1 
+	
 COMMIT TRAN
 GO
 
-EXEC PROC_DIRTYREAD_T2_LANG 'dish_5'
+--EXEC PROC_DIRTYREAD_T2_LANG 'dish_5'
 
 
 
 --Lam
 -- Mô tả: Quản lý cửa hàng cập nhật position cho nhân viên cấp dưới (chưa commit) thì nhân viên khác vào xem thông tin của nhân viên được cập nhật
 
--- T1: Quản lý cập nhật loại món ăn @type_dish của món có id là @id_dish
+-- T1: Quản lý cửa hàng cập nhật position cho nhân viên cấp dưới
 IF OBJECT_ID('PROC_DIRTYREAD_T1_LAM', 'p') IS NOT NULL DROP PROC PROC_DIRTYREAD_T1_LAM
 GO
 CREATE PROC PROC_DIRTYREAD_T1_LAM @id_employee nchar(10), @id_position nchar(10)
@@ -207,10 +209,10 @@ GO
 CREATE PROC PROC_DIRTYREAD_T2_ANHOA @id_agency nchar(10)
 AS
 BEGIN TRAN
-SET TRAN ISOLATION LEVEL READ UNCOMMITTED  --Đảm bảo cho việc xảy ra DirtyRead
-	SELECT *
-	FROM MENU
-	WHERE id_agency = @id_agency
+SET TRAN ISOLATION LEVEL READ UNCOMMITTED  -- Đảm bảo cho việc xảy ra DirtyRead
+	SELECT M.id_agency, D.id_dish, M.unit, D.dish_name, D.type_dish, D.image, D.price
+	FROM MENU M JOIN DISH D 
+	ON id_agency = @id_agency AND M.id_dish = D.id_dish AND M.isActive = 1
 COMMIT TRAN
 
 EXEC PROC_DIRTYREAD_T2_ANHOA 'ag_1'
@@ -222,9 +224,9 @@ CREATE PROC PROC_DIRTYREAD_T2_ANHOA @id_agency nchar(10)
 AS
 BEGIN TRAN
 SET TRAN ISOLATION LEVEL READ COMMITTED  --Giải quyết lỗi DirtyRead
-	SELECT *
-	FROM MENU
-	WHERE id_agency = @id_agency
+	SELECT M.id_agency, D.id_dish, M.unit, D.dish_name, D.type_dish, D.image, D.price
+	FROM MENU M JOIN DISH D 
+	ON id_agency = @id_agency AND M.id_dish = D.id_dish AND M.isActive = 1
 COMMIT TRAN
 EXEC PROC_DIRTYREAD_T2_ANHOA 'ag_1'
 --DUC
