b4d4a9aaff10b828b1b9fa45be75908249a46c23
truonglam103 <truonglam103@gmail.com>
2019-12-18 22:40:01 +0700

    update proc
    update proc

--- Data/UnrepeatableRead.sql
+++ Data/UnrepeatableRead.sql
@@ -14,7 +14,7 @@ BEGIN TRAN
 	SELECT M.id_agency, D.id_dish, M.unit, D.dish_name, D.type_dish, D.image, D.price
 	FROM MENU M JOIN DISH D
 	ON id_agency = @id_agency AND M.id_dish = D.id_dish AND M.isActive =1
-	WAITFOR DELAY '00:00:15'
+	WAITFOR DELAY '00:00:05'
 
 
 	SELECT M.id_agency, D.id_dish, M.unit, D.dish_name, D.type_dish, D.image, D.price
@@ -26,13 +26,15 @@ COMMIT TRAN
 --TRANSACTION 2--
 IF OBJECT_ID('PROC_UNREPEATABLEREAD_T2_CHUAN', 'P') IS NOT NULL DROP PROC PROC_UNREPEATABLEREAD_T2_CHUAN
 GO
-CREATE PROC PROC_UNREPEATABLEREAD_T2_CHUAN @id_dish nchar(10), @price nvarchar(50)
+CREATE PROC PROC_UNREPEATABLEREAD_T2_CHUAN @id_dish nchar(10), @price int
 AS
 BEGIN TRAN
 	UPDATE DISH 
 	SET [price] = @price
 	WHERE id_dish = @id_dish and isActive = 1 
 COMMIT TRAN
+
+exec PROC_UNREPEATABLEREAD_T2_CHUAN 'dish_1', 69
 
 --FIX =>TRANSACTION 1--
 IF OBJECT_ID('PROC_UNREPEATABLEREAD_T1_CHUAN', N'P') IS NOT NULL DROP PROC PROC_UNREPEATABLEREAD_T1_CHUAN
